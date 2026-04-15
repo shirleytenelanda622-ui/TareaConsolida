c3fce7d66fd75f78037faa7512dd5ded1252ed51
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-04-14 23:20:06 -0500

    Pruebas y validaciones del sistema

--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,7 @@
 //2. Sistema de atención y control de pacientes
+//ENUNCIADO
+//Una clínica privada requiere desarrollar un sistema para registrar información de sus pacientes y generar un reporte de atención médica.
+
 public class Main2 {
     public static void main(String[] args){
         Paciente pa1 = new Paciente("Carlos Perez", 0102030405, "Premium", 45, 25.50,3,4);
