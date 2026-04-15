a01635b8e83c0dbcef4a9b32e90f35e0cf0dc548
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-04-14 23:21:22 -0500

    Ejecucion del sistema

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 // 1.	Control de facturación y desempeño de ventas
-
+//Enunciado
+/*Una empresa comercial desea desarrollar un sistema para registrar información de sus vendedores y analizar el resultado de una venta realizada.
+    De cada vendedor interesa conocer su nombre, su área de trabajo, el monto total vendido, el porcentaje de comisión y el cumplimiento de meta.
+*/
 public class Main {
     public static void main(String[] args) {
         Vendedor ven1 = new Vendedor("Andrea Lopez", "Tecnologia", 2850.75, 8, 92);
